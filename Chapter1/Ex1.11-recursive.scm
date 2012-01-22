97ac3a5264e7635e55f583a066ab5fc9cc0a9c9c
Mick Brooks <mick.brooks@sinking.in>
2012-01-22 23:24:59 +0000

    Recursive solution for Exercise 1.11

---
+++ Chapter1/Ex1.11-recursive.scm
@@ -0,0 +1,8 @@
+#! /usr/bin/guile -s
+!#
+; coding: utf-8
+(define (f n)
+  (cond ((< n 3) n)
+        (else (+ (f (- n 1))
+                 (* 2 (f (- n 2)))
+                 (* 3 (f (- n 3)))))))
