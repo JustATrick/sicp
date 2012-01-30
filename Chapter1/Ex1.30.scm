36c8566c1a999520e034f8d246dc6a8408856599
Mick Brooks <mick.brooks@sinking.in>
2012-01-30 22:05:06 +0000

    Answer for Exercise 1.30, with an unanswered question on accuracy as n increases

---
+++ Chapter1/Ex1.30.scm
@@ -0,0 +1,9 @@
+#! /usr/bin/guile -s
+!#
+; coding: utf-8
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ (term a) result))))
+  (iter a 0))
