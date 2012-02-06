cee17270e833492c44719b647efc7b5d93832fce
Mick Brooks <mick.brooks@sinking.in>
2012-02-06 22:54:20 +0000

    Answer for Exercise 2.31.

---
+++ Chapter2/Ex2.31.scm
@@ -0,0 +1,9 @@
+#! /usr/bin/guile -s
+!#
+; coding: utf-8
+(define (tree-map proc tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (tree-map proc sub-tree)
+             (proc sub-tree)))
+       tree))
